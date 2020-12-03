f83182fbd9934af9dad0f1badd0dbf20ee58d6fd
Greg Rubin <rubin@amazon.com>
2020-12-02 21:34:07 -0800

    Complete day 3

---
+++ src/day3.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+
+use crate::*;
+
+#[derive(Debug)]
+struct TreeMap {
+    width: usize,
+    height: usize,
+    trees: HashMap<(usize, usize), char>
+}
+
+impl TreeMap {
+    fn parse(file_name: &str) -> TreeMap {
+        let lines = read_file(file_name);
+        let mut width = 0;
+        let mut height = 0;
+        let mut trees = HashMap::new();
+        for line in lines {
+            width = 0;
+            let line = line.unwrap();
+            let line = line.trim();
+            for unit in line.chars() {
+                trees.insert((width, height), unit);
+                width += 1;
+            }
+            height += 1;
+        }
+        TreeMap { width, height, trees }
+    }
+
+    fn get_plant(&self, x: usize, y: usize) -> &char {
+        if y >= self.height {
+            return &' ';
+        }
+
+        self.trees.get(&(x % self.width, y)).unwrap()
+    }
+
+    fn count_trees(&self, x_diff: usize, y_diff: usize) -> u32 {
+        let mut tree_count = 0;
+        let mut x = 0;
+        let mut y = 0;
+        loop {
+            tree_count += match self.get_plant(x, y) {
+                '.' => 0,
+                '#' => 1,
+                _ => break
+            };
+            x += x_diff;
+            y += y_diff;
+        }
+        tree_count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day3_smoke() {
+        let map = TreeMap::parse("day3_smoke.txt");
+
+        println!("Day3 Smoke: {}", map.count_trees(3, 1));
+
+    }
+
+    #[test]
+    fn day3_1() {
+        let map = TreeMap::parse("day3.txt");
+        println!("Day3.1: {}", map.count_trees(3, 1));
+    }
+
+    #[test]
+    fn day3_2() {
+        let map = TreeMap::parse("day3.txt");
+        
+        let mut product : u64 = 1;
+        product *= map.count_trees(1, 1) as u64;
+        product *= map.count_trees(3, 1) as u64;
+        product *= map.count_trees(5, 1) as u64;
+        product *= map.count_trees(7, 1) as u64;
+        product *= map.count_trees(1, 2) as u64;
+        println!("Day3.2: {}", product);
+    }
+}
